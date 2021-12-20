7bf0c4b384f59f4c17d9df8041825b7ed2c44465
ReproEng <stefan.klessinger@uni-passau.de>
2021-12-20 17:11:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,15 @@ project=$1
 image_name=$2
 res_path="output/${project}"
 
+# Create output path and ensure permissions are (more than) sufficient
 rm -rf ${res_path}
 mkdir -p -m 777 $res_path
-chmod 777 $res_path
-docker build --build-arg project=$project -t meta .
 
+# Build and run container with mounted output directory
+docker build --build-arg project=$project -t meta .
 docker run -it -v "$(pwd)"/${res_path}:/home/meta/output meta
-echo "$(pwd)"/${res_path}
+
+# Build and save Docker image
 cd $res_path
 docker build -t ${image_name} .
 docker save ${image_name} > ${image_name}.tar
